c5d89d659d3b6d438a376d9f3ba46f690187d557
Salieri23 <lopezperezandres800@gmail.com>
2025-11-27 11:50:30 -0500

    Update ControladorVistaEmpleados.java

--- src/main/java/com/panaderia/controller/ControladorVistaEmpleados.java
+++ src/main/java/com/panaderia/controller/ControladorVistaEmpleados.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @Controller
 @RequestMapping("/empleados") // URL más clara para la gestión de empleados
-public class EmpleadoController {
+public class ControladorVistaEmpleados {
 
     @Autowired
     private EmpleadoRepository empleadoRepository;
